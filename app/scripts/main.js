2f50f1d22185eb25df80b7794c191fdde50735d7
mjuniper <mjuniper@gmail.com>
2014-03-04 14:20:42 -0700

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global backboneDemoDs2014, $*/
+
+
+window.backboneDemoDs2014 = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    backboneDemoDs2014.init();
+});
