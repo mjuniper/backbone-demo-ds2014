81ef6f76d8f175151fe04f7cfc9f4741eda0ed0c
mjuniper <mjuniper@gmail.com>
2014-03-04 20:31:47 -0700

    Updates

--- scripts/e208b856.main.js
+++
@@ -1,1 +0,0 @@
-window.backboneDemoDs2014={Models:{},Collections:{},Views:{},Routers:{},init:function(){"use strict";console.log("Hello from Backbone!")}},$(document).ready(function(){"use strict";backboneDemoDs2014.init()}),this.JST=this.JST||{};
