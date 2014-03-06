c40eeb9bd2310f1c381e9d90bbcdec82e9c5214d
mjuniper <mjuniper@gmail.com>
2014-03-06 13:45:44 -0700

    trying to get the darn tests running

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,204 +1,201 @@
-/*global backboneDemoDs2014, $*/
+/*global backboneDemoDs2014, $, Backbone, _, esri, dojo*/
 
 //http://developers.arcgis.com/javascript/samples/data_gas_prices/
 
 if (!this.backboneDemoDs2014 || typeof this.backboneDemoDs2014 !== 'object') {
-    this.backboneDemoDs2014 = {};
+  this.backboneDemoDs2014 = {};
 }
 
+(function(){
+  'use strict';
 
-backboneDemoDs2014.GasPriceModel = Backbone.Model.extend({
-  getDisplayValue: function () {
-    var regular = this.get('regular');
-    return parseFloat(parseFloat(regular.replace('$', '')).toFixed(2));
-  }
-})
-
-
-backboneDemoDs2014.GasPricesCollection = Backbone.Collection.extend({
-  model: backboneDemoDs2014.GasPriceModel,
-
-  url: 'http://apify.heroku.com/api/gasprices.json',
-
-  fetch: function (options) {
-    //override fetch to use jsonp
-    options = options || {};
-    options.dataType = 'jsonp';
-    return Backbone.Collection.prototype.fetch.apply(this, [options]);
-  },
-
-  parse: function (response, options) {
-    return JSON.parse(response);
-  },
-
-  getMin: function () {
-    var model = this.min(function (item) { return item.getDisplayValue(); });
-    return model.getDisplayValue();
-  },
-
-  getMax: function () {
-    var model = this.max(function (item) { return item.getDisplayValue(); });
-    return model.getDisplayValue();
-  },
-
-  findGasPrice: function (stateName) {
-    var model = this.findWhere({ state: stateName });
-    return model.getDisplayValue();
-  }
-})
-
-
-backboneDemoDs2014.LegendView = Backbone.View.extend({
-  initialize: function (options) {
-    _.bindAll(this);
-
-    this.legend = new esri.dijit.Legend({
-      map: options.map,
-      layerInfos: [{ 'layer': options.fl, 'title': 'Regular Gas' }]
-    },'legend');
-    this.legend.startup();
-
-    this.tipTemplate = _.template('<strong><%= STATE_NAME %>:  $<%= GAS_DISPLAY %></strong>');
-
-    options.fl.on('mouse-over', this.showTip);
-    options.fl.on('mouse-out', this.hideTip);
-  },
-
-  el: '#legend',
-
-  showTip: function (e) {
-    this.$('.tip').html(this.tipTemplate(e.graphic.attributes));
-  },
-
-  hideTip: function () {
-    this.$('.tip').empty();
-  }
-});
-
-
-backboneDemoDs2014.MapView = Backbone.View.extend({
-  initialize: function () {
-    _.bindAll(this);
-    var bounds = new esri.geometry.Extent({'xmin':-2332499,'ymin':-1530060,'xmax':2252197,'ymax':1856904,'spatialReference':{'wkid':102003}});
-    this.map = new esri.Map('map', { 
-      extent: bounds,
-      lods: [{'level':0, 'resolution': 3966, 'scale': 15000000}],
-      slider: false
-    });
-
-    this.fl = new esri.layers.FeatureLayer('http://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3', {
-      maxAllowableOffset: this.map.extent.getWidth() / this.map.width,
-      mode: esri.layers.FeatureLayer.MODE_SNAPSHOT,
-      outFields: ['STATE_NAME'],
-      visible: true
-    });
-    // override default renderer so that states aren't drawn
-    // until the gas price data has been loaded and is joined
-    this.fl.setRenderer(new esri.renderer.SimpleRenderer(null));
-
-    this.updateEndHandler = this.fl.on('update-end', this.onUpdateEnd);
-
-    this.map.addLayer(this.fl);
-
-    this.gasPricesCollection = new backboneDemoDs2014.GasPricesCollection();
-  },
-
-  el: '#map',
-
-  onUpdateEnd: function () {
-    // get gas price data
-    this.updateEndHandler.remove();
-    this.gasPricesCollection.fetch()
-      .done(this.drawFeatureLayer)
-      .fail(this.pricesError);
-  },
-
-  drawFeatureLayer: function (data) {
-    var gasMin = this.gasPricesCollection.getMin(); 
-    var gasMax = this.gasPricesCollection.getMax();
-
-    // add an attribute to each attribute so gas price is displayed
-    // on mouse over below the legend
-    var displayValue;
-    _.each(this.fl.graphics, function(g) {
-      displayValue = this.gasPricesCollection.findWhere({ state: g.attributes.STATE_NAME }).getDisplayValue();
-      g.attributes.GAS_DISPLAY = displayValue;
-    }, this);
-
-    // create a class breaks renderer
-    var breaks = this.calcBreaks(gasMin, gasMax, 4);
-    // console.log('gas price breaks: ', breaks);
-    var sfs = esri.symbol.SimpleFillSymbol;
-    var sls = esri.symbol.SimpleLineSymbol;
-    var outline = sls('solid', new dojo.Color('#444'), 1);
-    var br = new esri.renderer.ClassBreaksRenderer(null, this.findGasPrice);
-    br.setMaxInclusive(true);
-    br.addBreak(breaks[0], breaks[1], new sfs('solid', outline, new dojo.Color([255, 255, 178, 0.75])));
-    br.addBreak(breaks[1], breaks[2], new sfs('solid', outline, new dojo.Color([254, 204, 92, 0.75])));
-    br.addBreak(breaks[2], breaks[3], new sfs('solid', outline, new dojo.Color([253, 141, 60, 0.75])));
-    br.addBreak(breaks[3], gasMax, new sfs('solid', outline, new dojo.Color([227, 26, 28, 0.75])));
-
-    this.fl.setRenderer(br);
-    this.fl.redraw();
-
-    this.legend = new backboneDemoDs2014.LegendView({ map: this.map, fl: this.fl });
-
-    // remove the loading div
-    $('#loading').remove();
-  },
-
-  findGasPrice: function (graphic) {
-    return this.gasPricesCollection.findGasPrice(graphic.attributes.STATE_NAME);
-  },
-
-  calcBreaks: function (min, max, numberOfClasses) {
-    var range = (max - min) / numberOfClasses;
-    var breakValues = [];
-    for ( var i = 0; i < numberOfClasses; i++ ) {
-      breakValues.push(this.formatDollars(min + ( range * i )));
+  backboneDemoDs2014.GasPriceModel = Backbone.Model.extend({
+    getDisplayValue: function () {
+      var regular = this.get('regular');
+      return parseFloat(parseFloat(regular.replace('$', '')).toFixed(2));
     }
-    // console.log('break values: ', breakValues);
-    return breakValues;
-  },
+  });
 
-  formatDollars: function (num) {
-    return dojo.number.format(num, { 'places': 2 });
-  },
-  
-  pricesError: function (e) {
-    console.log('error getting gas price data: ', e);
-  }
-})
+
+  backboneDemoDs2014.GasPricesCollection = Backbone.Collection.extend({
+    model: backboneDemoDs2014.GasPriceModel,
+
+    url: 'http://apify.heroku.com/api/gasprices.json',
+
+    fetch: function (options) {
+      //override fetch to use jsonp
+      options = options || {};
+      options.dataType = 'jsonp';
+      return Backbone.Collection.prototype.fetch.apply(this, [options]);
+    },
+
+    parse: function (response, options) {
+      return JSON.parse(response);
+    },
+
+    getMin: function () {
+      var model = this.min(function (item) { return item.getDisplayValue(); });
+      return model.getDisplayValue();
+    },
+
+    getMax: function () {
+      var model = this.max(function (item) { return item.getDisplayValue(); });
+      return model.getDisplayValue();
+    },
+
+    findGasPrice: function (stateName) {
+      var model = this.findWhere({ state: stateName });
+      return model.getDisplayValue();
+    }
+  });
+
+
+  backboneDemoDs2014.LegendView = Backbone.View.extend({
+    initialize: function (options) {
+      _.bindAll(this);
+
+      this.legend = new esri.dijit.Legend({
+        map: options.map,
+        layerInfos: [{ 'layer': options.fl, 'title': 'Regular Gas' }]
+      },'legend');
+      this.legend.startup();
+
+      this.tipTemplate = _.template('<strong><%= STATE_NAME %>:  $<%= GAS_DISPLAY %></strong>');
+
+      options.fl.on('mouse-over', this.showTip);
+      options.fl.on('mouse-out', this.hideTip);
+    },
+
+    el: '#legend',
+
+    showTip: function (e) {
+      this.$('.tip').html(this.tipTemplate(e.graphic.attributes));
+    },
+
+    hideTip: function () {
+      this.$('.tip').empty();
+    }
+  });
+
+
+  backboneDemoDs2014.MapView = Backbone.View.extend({
+    initialize: function () {
+      _.bindAll(this);
+      var bounds = new esri.geometry.Extent({'xmin':-2332499,'ymin':-1530060,'xmax':2252197,'ymax':1856904,'spatialReference':{'wkid':102003}});
+      this.map = new esri.Map('map', {
+        extent: bounds,
+        lods: [{'level':0, 'resolution': 3966, 'scale': 15000000}],
+        slider: false
+      });
+
+      this.fl = new esri.layers.FeatureLayer('http://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3', {
+        maxAllowableOffset: this.map.extent.getWidth() / this.map.width,
+        mode: esri.layers.FeatureLayer.MODE_SNAPSHOT,
+        outFields: ['STATE_NAME'],
+        visible: true
+      });
+      // override default renderer so that states aren't drawn
+      // until the gas price data has been loaded and is joined
+      this.fl.setRenderer(new esri.renderer.SimpleRenderer(null));
+
+      this.updateEndHandler = this.fl.on('update-end', this.onUpdateEnd);
+
+      this.map.addLayer(this.fl);
+
+      this.gasPricesCollection = new backboneDemoDs2014.GasPricesCollection();
+    },
+
+    el: '#map',
+
+    onUpdateEnd: function () {
+      // get gas price data
+      this.updateEndHandler.remove();
+      this.gasPricesCollection.fetch()
+        .done(this.drawFeatureLayer)
+        .fail(this.pricesError);
+    },
+
+    drawFeatureLayer: function (data) {
+      var gasMin = this.gasPricesCollection.getMin();
+      var gasMax = this.gasPricesCollection.getMax();
+
+      // add an attribute to each attribute so gas price is displayed
+      // on mouse over below the legend
+      var displayValue;
+      _.each(this.fl.graphics, function(g) {
+        displayValue = this.gasPricesCollection.findWhere({ state: g.attributes.STATE_NAME }).getDisplayValue();
+        g.attributes.GAS_DISPLAY = displayValue;
+      }, this);
+
+      // create a class breaks renderer
+      var breaks = this.calcBreaks(gasMin, gasMax, 4);
+      // console.log('gas price breaks: ', breaks);
+      var SFS = esri.symbol.SimpleFillSymbol;
+      var SLS = esri.symbol.SimpleLineSymbol;
+      var outline = SLS('solid', new dojo.Color('#444'), 1);
+      var br = new esri.renderer.ClassBreaksRenderer(null, this.findGasPrice);
+      br.setMaxInclusive(true);
+      br.addBreak(breaks[0], breaks[1], new SFS('solid', outline, new dojo.Color([255, 255, 178, 0.75])));
+      br.addBreak(breaks[1], breaks[2], new SFS('solid', outline, new dojo.Color([254, 204, 92, 0.75])));
+      br.addBreak(breaks[2], breaks[3], new SFS('solid', outline, new dojo.Color([253, 141, 60, 0.75])));
+      br.addBreak(breaks[3], gasMax, new SFS('solid', outline, new dojo.Color([227, 26, 28, 0.75])));
+
+      this.fl.setRenderer(br);
+      this.fl.redraw();
+
+      this.legend = new backboneDemoDs2014.LegendView({ map: this.map, fl: this.fl });
+
+      // remove the loading div
+      $('#loading').remove();
+    },
+
+    findGasPrice: function (graphic) {
+      return this.gasPricesCollection.findGasPrice(graphic.attributes.STATE_NAME);
+    },
+
+    calcBreaks: function (min, max, numberOfClasses) {
+      var range = (max - min) / numberOfClasses;
+      var breakValues = [];
+      for ( var i = 0; i < numberOfClasses; i++ ) {
+        breakValues.push(this.formatDollars(min + ( range * i )));
+      }
+      // console.log('break values: ', breakValues);
+      return breakValues;
+    },
+
+    formatDollars: function (num) {
+      return dojo.number.format(num, { 'places': 2 });
+    },
+    
+    pricesError: function (e) {
+      console.log('error getting gas price data: ', e);
+    }
+  });
+})();
 
 
 $(function () {
   'use strict';
   require([
-        'dojo/json', 
-        'dojo/_base/connect', 
-        'dojo/_base/Color', 
-        'dojo/number', 
-        
-        'esri/map', 
-        'esri/geometry/Extent',
-        'esri/symbols/SimpleLineSymbol',
-        'esri/symbols/SimpleFillSymbol',
-        'esri/renderers/SimpleRenderer',
-        'esri/renderers/ClassBreaksRenderer',
+    'dojo/json',
+    'dojo/_base/connect',
+    'dojo/_base/Color',
+    'dojo/number',
+    
+    'esri/map',
+    'esri/geometry/Extent',
+    'esri/symbols/SimpleLineSymbol',
+    'esri/symbols/SimpleFillSymbol',
+    'esri/renderers/SimpleRenderer',
+    'esri/renderers/ClassBreaksRenderer',
 
-        'esri/layers/FeatureLayer', 
-        'esri/dijit/Legend', 
-        'esri/request',
- 
-        'dojo/domReady!'
-      ], function(
-        JSON, conn, Color, number, 
-        Map, Extent, SimpleLineSymbol, SimpleFillSymbol, SimpleRenderer, ClassBreaksRenderer, 
-        FeatureLayer, Legend, esriRequest) {
+    'esri/layers/FeatureLayer',
+    'esri/dijit/Legend',
+    'esri/request',
 
-          var mapView = new backboneDemoDs2014.MapView();
-
-        }
-      );
-
+    'dojo/domReady!'
+  ], function() {
+        var mapView = new backboneDemoDs2014.MapView();
+      }
+    );
 });
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,37 @@
-/*global describe, it */
-'use strict';
-(function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+/*global backboneDemoDs2014, expect, describe, before, it */
 
-            });
-        });
+(function () {
+  'use strict';
+
+  describe('GasPriceModel', function () {
+    describe('getDisplayValue', function () {
+      var model;
+      before(function () {
+        model = new backboneDemoDs2014.GasPriceModel();
+      });
+
+      it('should return a number with 2 deciaml places and no $', function(){
+        model.set('regular', '$2.145');
+        expect(model.getDisplayValue()).to.eql(2.16);
+      });
     });
+  });
+
+  describe('GasPricesCollection', function () {
+    
+    describe('getMin, getMax', function () {
+      var coll;
+      before(function () {
+        var json = '[{\"state\":\"State\",\"regular\":\"Regular\",\"premium\":\"Premium\",\"mid\":\"Mid\",\"diesel\":\"Premium\"},{\"state\":\"Alaska\",\"regular\":\"$3.802\",\"premium\":\"$3.963\",\"mid\":\"$3.861\",\"diesel\":\"$3.963\"},{\"state\":\"Alabama\",\"regular\":\"$3.210\",\"premium\":\"$3.617\",\"mid\":\"$3.419\",\"diesel\":\"$3.617\"},{\"state\":\"Arkansas\",\"regular\":\"$3.239\",\"premium\":\"$3.577\",\"mid\":\"$3.391\",\"diesel\":\"$3.577\"},{\"state\":\"Arizona\",\"regular\":\"$3.336\",\"premium\":\"$3.593\",\"mid\":\"$3.459\",\"diesel\":\"$3.593\"},{\"state\":\"California\",\"regular\":\"$3.881\",\"premium\":\"$4.077\",\"mid\":\"$3.979\",\"diesel\":\"$4.077\"},{\"state\":\"Colorado\",\"regular\":\"$3.573\",\"premium\":\"$3.844\",\"mid\":\"$3.707\",\"diesel\":\"$3.844\"},{\"state\":\"Connecticut\",\"regular\":\"$3.760\",\"premium\":\"$4.082\",\"mid\":\"$3.922\",\"diesel\":\"$4.082\"},{\"state\":\"District of Columbia\",\"regular\":\"$3.524\",\"premium\":\"$3.957\",\"mid\":\"$3.818\",\"diesel\":\"$3.957\"},{\"state\":\"Delaware\",\"regular\":\"$3.487\",\"premium\":\"$3.900\",\"mid\":\"$3.720\",\"diesel\":\"$3.900\"},{\"state\":\"Florida\",\"regular\":\"$3.451\",\"premium\":\"$3.866\",\"mid\":\"$3.678\",\"diesel\":\"$3.866\"},{\"state\":\"Georgia\",\"regular\":\"$3.292\",\"premium\":\"$3.667\",\"mid\":\"$3.483\",\"diesel\":\"$3.667\"},{\"state\":\"Hawaii\",\"regular\":\"$4.108\",\"premium\":\"$4.297\",\"mid\":\"$4.205\",\"diesel\":\"$4.297\"},{\"state\":\"Iowa\",\"regular\":\"$3.495\",\"premium\":\"$3.710\",\"mid\":\"$3.384\",\"diesel\":\"$3.710\"},{\"state\":\"Idaho\",\"regular\":\"$3.319\",\"premium\":\"$3.549\",\"mid\":\"$3.429\",\"diesel\":\"$3.549\"},{\"state\":\"Illinois\",\"regular\":\"$3.709\",\"premium\":\"$4.144\",\"mid\":\"$3.912\",\"diesel\":\"$4.144\"},{\"state\":\"Indiana\",\"regular\":\"$3.585\",\"premium\":\"$3.828\",\"mid\":\"$3.707\",\"diesel\":\"$3.828\"},{\"state\":\"Kansas\",\"regular\":\"$3.373\",\"premium\":\"$3.656\",\"mid\":\"$3.489\",\"diesel\":\"$3.656\"},{\"state\":\"Kentucky\",\"regular\":\"$3.400\",\"premium\":\"$3.659\",\"mid\":\"$3.534\",\"diesel\":\"$3.659\"},{\"state\":\"Louisiana\",\"regular\":\"$3.247\",\"premium\":\"$3.610\",\"mid\":\"$3.438\",\"diesel\":\"$3.610\"},{\"state\":\"Massachusetts\",\"regular\":\"$3.544\",\"premium\":\"$3.843\",\"mid\":\"$3.709\",\"diesel\":\"$3.843\"},{\"state\":\"Maryland\",\"regular\":\"$3.471\",\"premium\":\"$3.870\",\"mid\":\"$3.691\",\"diesel\":\"$3.870\"},{\"state\":\"Maine\",\"regular\":\"$3.610\",\"premium\":\"$3.870\",\"mid\":\"$3.728\",\"diesel\":\"$3.870\"},{\"state\":\"Michigan\",\"regular\":\"$3.671\",\"premium\":\"$3.962\",\"mid\":\"$3.811\",\"diesel\":\"$3.962\"},{\"state\":\"Minnesota\",\"regular\":\"$3.466\",\"premium\":\"$3.791\",\"mid\":\"$3.580\",\"diesel\":\"$3.791\"},{\"state\":\"Missouri\",\"regular\":\"$3.288\",\"premium\":\"$3.600\",\"mid\":\"$3.432\",\"diesel\":\"$3.600\"},{\"state\":\"Mississippi\",\"regular\":\"$3.208\",\"premium\":\"$3.586\",\"mid\":\"$3.385\",\"diesel\":\"$3.586\"},{\"state\":\"Montana\",\"regular\":\"$3.206\",\"premium\":\"$3.463\",\"mid\":\"$3.306\",\"diesel\":\"$3.463\"},{\"state\":\"North Carolina\",\"regular\":\"$3.357\",\"premium\":\"$3.757\",\"mid\":\"$3.550\",\"diesel\":\"$3.757\"},{\"state\":\"North Dakota\",\"regular\":\"$3.483\",\"premium\":\"$3.816\",\"mid\":\"$3.567\",\"diesel\":\"$3.816\"},{\"state\":\"Nebraska\",\"regular\":\"$3.477\",\"premium\":\"$3.716\",\"mid\":\"$3.459\",\"diesel\":\"$3.716\"},{\"state\":\"New Hampshire\",\"regular\":\"$3.491\",\"premium\":\"$3.786\",\"mid\":\"$3.640\",\"diesel\":\"$3.786\"},{\"state\":\"New Jersey\",\"regular\":\"$3.390\",\"premium\":\"$3.717\",\"mid\":\"$3.579\",\"diesel\":\"$3.717\"},{\"state\":\"New Mexico\",\"regular\":\"$3.334\",\"premium\":\"$3.571\",\"mid\":\"$3.456\",\"diesel\":\"$3.571\"},{\"state\":\"Nevada\",\"regular\":\"$3.483\",\"premium\":\"$3.704\",\"mid\":\"$3.591\",\"diesel\":\"$3.704\"},{\"state\":\"New York\",\"regular\":\"$3.755\",\"premium\":\"$4.073\",\"mid\":\"$3.919\",\"diesel\":\"$4.073\"},{\"state\":\"Ohio\",\"regular\":\"$3.556\",\"premium\":\"$3.796\",\"mid\":\"$3.673\",\"diesel\":\"$3.796\"},{\"state\":\"Oklahoma\",\"regular\":\"$3.354\",\"premium\":\"$3.635\",\"mid\":\"$3.481\",\"diesel\":\"$3.635\"},{\"state\":\"Oregon\",\"regular\":\"$3.464\",\"premium\":\"$3.677\",\"mid\":\"$3.560\",\"diesel\":\"$3.677\"},{\"state\":\"Pennsylvania\",\"regular\":\"$3.641\",\"premium\":\"$3.939\",\"mid\":\"$3.770\",\"diesel\":\"$3.939\"},{\"state\":\"Rhode Island\",\"regular\":\"$3.581\",\"premium\":\"$3.903\",\"mid\":\"$3.759\",\"diesel\":\"$3.903\"},{\"state\":\"South Carolina\",\"regular\":\"$3.171\",\"premium\":\"$3.582\",\"mid\":\"$3.366\",\"diesel\":\"$3.582\"},{\"state\":\"South Dakota\",\"regular\":\"$3.471\",\"premium\":\"$3.780\",\"mid\":\"$3.434\",\"diesel\":\"$3.780\"},{\"state\":\"Tennessee\",\"regular\":\"$3.214\",\"premium\":\"$3.583\",\"mid\":\"$3.393\",\"diesel\":\"$3.583\"},{\"state\":\"Texas\",\"regular\":\"$3.224\",\"premium\":\"$3.575\",\"mid\":\"$3.408\",\"diesel\":\"$3.575\"},{\"state\":\"Utah\",\"regular\":\"$3.313\",\"premium\":\"$3.536\",\"mid\":\"$3.427\",\"diesel\":\"$3.536\"},{\"state\":\"Virginia\",\"regular\":\"$3.253\",\"premium\":\"$3.677\",\"mid\":\"$3.484\",\"diesel\":\"$3.677\"},{\"state\":\"Vermont\",\"regular\":\"$3.620\",\"premium\":\"$3.988\",\"mid\":\"$3.801\",\"diesel\":\"$3.988\"},{\"state\":\"Washington\",\"regular\":\"$3.495\",\"premium\":\"$3.714\",\"mid\":\"$3.604\",\"diesel\":\"$3.714\"},{\"state\":\"Wisconsin\",\"regular\":\"$3.567\",\"premium\":\"$3.974\",\"mid\":\"$3.730\",\"diesel\":\"$3.974\"},{\"state\":\"West Virginia\",\"regular\":\"$3.504\",\"premium\":\"$3.762\",\"mid\":\"$3.628\",\"diesel\":\"$3.762\"},{\"state\":\"Wyoming\",\"regular\":\"$3.347\",\"premium\":\"$3.603\",\"mid\":\"$3.468\",\"diesel\":\"$3.603\"}]';
+        var data = JSON.parse(json);
+        coll = new backboneDemoDs2014.GasPricesCollection(data);
+      });
+
+      it('should return a number with 2 deciaml places and no $', function(){
+        
+      });
+    });
+
+  });
+
 })();
