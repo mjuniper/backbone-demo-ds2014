3306fa6fbcf624eb42be1e21a7b36b5ef2ba7b2a
mjuniper <mjuniper@gmail.com>
2014-03-05 22:06:03 -0700

    getting it working and backbone-ifying it

--- Gruntfile.js
+++ Gruntfile.js
@@ -229,8 +229,7 @@ module.exports = function (grunt) {
                         '*.{ico,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/{,*/}*.*',
-                        'extras/*'
+                        'styles/fonts/{,*/}*.*'
                     ]
                 }]
             }
